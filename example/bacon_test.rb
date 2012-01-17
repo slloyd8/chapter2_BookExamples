ac1b61959e8967cd2cfd4cc523aeba2f3c23dc1e
Stephen Lloyd <stephenlloyd@windermeresolutions.com>
2012-01-16 16:32:57 -0800

    upload new files

---
+++ example/bacon_test.rb
@@ -0,0 +1,12 @@
+require 'test/unit'
+class Bacon
+  def self.saved?
+    false
+  end
+end
+
+class BaconTest < Test::Unit::TestCase
+  def test_saved
+    assert Bacon.saved? , "Our bacon was not saved :("
+  end
+end
